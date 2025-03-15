d8ba8827d120b4f13894776430ed3ae16fdd6506
Lawrence Whiteside <law@cinemasetfree.com>
2025-03-15 14:34:31 -0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,16 @@ export default defineConfig({
       entry: "src/index.ts",
       name: "Arcade",
       // the proper extensions will be added
-      fileName: "arcade",
+      fileName: "index",
+    },
+    commonjsOptions: {
+      include: ["node_modules/**/*.js"],
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       plugins: [nodePolyfills(/* options */)],
       // into your library
-      external: ["@clack/prompts", "fs", "path"],
+      external: ["fs", "path"],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
